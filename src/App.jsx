e7ff31a03dd27c344c867524d1bc58a07b08ef3b
Kyhtera <ahmetuslum@gmail.com>
2023-03-13 17:27:09 +0300

    Api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
+import Pages from "./pages/Pages";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>this is recipe app</h1>
+      <Pages />
 
     </div>
   );
