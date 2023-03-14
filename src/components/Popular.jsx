03d5a4ca861ab3da567a0c299427ed494e5ad454
Kyhtera <ahmetuslum@gmail.com>
2023-03-14 16:51:10 +0300

    added wrapper

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,20 +1,53 @@
 
 import React from 'react'
-import {useEffect} from 'react'
+import {useState, useEffect} from 'react'
+import styled from "styled-components"
+
 
 function Popular() {
+        const [popular, setPopular] = useState([]);
         useEffect (() =>{
             getPopular();
         }, []);
         const getPopular = async  () => {
         const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
         const data = await api.json();
+        setPopular(data.recipes);
         console.log(data);
     }
 
-  return (
-    <div>Popular</div>
-  )
+  return <div>
+      {popular.map((recipe) => {
+        return(
+          <Wrapper>
+            <h3>Popular Picks</h3>
+            {popular.map((recipe) => {
+              return(
+                  <Card>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                  </Card>
+              )
+            })}
+          </Wrapper>
+        );
+      })}
+
+    </div>;
+  
 }
 
+const Wrapper = styled.div`
+margin : 4rem 0 rem`;
+
+
+const Card = styled.div `
+min-height:25rem;
+border-radius:2rem;
+overflow: hidden;
+
+img{
+  border-radius:2rem;
+}
+`;
 export default Popular
